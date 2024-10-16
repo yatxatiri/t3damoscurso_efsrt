b24d484475e1ffb088b6b669da2e6e558d9d6ed5
Jose Guillermo <velasquezperezjg@gmail.com>
2024-10-15 21:27:08 -0500

    Creación de proyecto Spring son Maven usando el patrón MVC

---
+++ src/main/java/com/t3damoscurso/T3damoscursoApplication.java
@@ -0,0 +1,13 @@
+package com.t3damoscurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3damoscursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3damoscursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t3damoscurso/T3damoscursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3damoscurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3damoscursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
